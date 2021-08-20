ae2af77a1a36e792db8f9f264e596d852dfa589f
this-is-bennyk <65842636+this-is-bennyk@users.noreply.github.com>
2021-08-19 23:23:33 -0700

    Delete test directory

--- test/BF RIG.gd
+++
@@ -1,5 +0,0 @@
-extends Spatial
-
-func _ready():
-	for i in $"Boyfriend/Boyfriend Armature/Skeleton".get_bone_count():
-		print($"Boyfriend/Boyfriend Armature/Skeleton".get_bone_name(i) + " parent: " + str($"Boyfriend/Boyfriend Armature/Skeleton".get_bone_parent(i)))
